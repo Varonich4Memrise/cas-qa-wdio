e4b920d3aa3c5c6c26408112a365b34c0ff86eef
alesiavaronich <alesia.varonich@memrise.com>
2023-01-18 13:53:08 +0300

    initial commit

---
+++ test/helper/apiHelper.ts
@@ -0,0 +1,10 @@
+import * as request from "supertest" 
+
+// (async function GET() {
+//     let res = await request("https://reqres.in")
+//     .get("/api/user?page=2");
+//     console.log(`Res: ${JSON.stringify(res.body)}`);
+// })()
+
+
+
